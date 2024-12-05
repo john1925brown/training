24ab58b689df0d05eb350093fcecbc204d937899
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-12-05 08:52:36 +0300

    objects tests

---
+++ src/training_tests/tests_02/tests__02.ts
@@ -0,0 +1,70 @@
+type TechnologyType = {
+  id: number;
+  title: string;
+};
+
+type LocalCityType = {
+  cityName: string;
+  country: string;
+};
+
+type StudentAddressType = {
+  street: string;
+  city: LocalCityType;
+};
+
+type StudentType = {
+  name: string;
+  age: number;
+  isActive: boolean;
+  address: StudentAddressType;
+  technologies: Array<TechnologyType>;
+};
+
+const student: StudentType = {
+  name: 'Dima',
+  age: 26,
+  isActive: true,
+  address: {
+    street: 'Surganova 2',
+    city: {
+      cityName: 'Minsk',
+      country: 'Belarus',
+    },
+  },
+  technologies: [
+    { id: 1, title: 'HTML' },
+    { id: 2, title: 'CSS' },
+    { id: 3, title: 'JS' },
+    { id: 4, title: 'REACT' },
+  ],
+};
+
+type StreetAddressType = {
+  title: string;
+};
+
+type AddressType = {
+  number?: number;
+  street: StreetAddressType;
+};
+
+type HousesType = {
+  buildedAt: number;
+  repaired: boolean;
+  address: AddressType;
+};
+
+type GovernmentBuildingsType = {
+  type: string
+  budget: number
+  staffCount: number
+  address: AddressType
+}
+
+export type CityType = {
+  title: string;
+  houses: Array<HousesType>;
+  governmentBuildings: Array<GovernmentBuildingsType>;
+  citizensNumber: number;
+};
