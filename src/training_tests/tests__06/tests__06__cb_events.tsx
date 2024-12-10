cd8e659f0c5edfadc879787ff7b6573a3e85c8be
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-12-10 20:18:26 +0300

    native js, callback

--- src/training_tests/tests__06/tests__06__cb_events.tsx
+++ src/training_tests/tests__06/tests__06__cb_events.tsx
@@ -5,6 +5,19 @@ const callback = (): number => {
 
 window.setTimeout(callback, 1000);
 
-const User = () => {
-  return <div></div>
-}
+export const User = () => {
+  const deleteUser = () => {
+    console.log('user has been deleted');
+  };
+
+  const saveUser = () => {
+    console.log('user has been saved');
+  };
+
+  return (
+    <div>
+      <button onClick={deleteUser}>delete</button>
+      <button onClick={saveUser}>save</button>
+    </div>
+  );
+};
